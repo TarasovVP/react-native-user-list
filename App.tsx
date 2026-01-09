262d5c3ae15502b5a2d6dfc1c1f5605e22a63e78
TarasovVP <vptarasov1998@gmail.com>
2026-01-09 14:47:18 +0200

    Update app tsx

--- App.tsx
+++ App.tsx
@@ -1,43 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React from 'react';
 import { UsersScreen } from './src/features/users/screens/UsersScreen';
-function App(): React.JSX.Element {
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-      <UsersScreen />
-    </SafeAreaView>
-  );
-}
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const App = () => {
+  return <UsersScreen />;
+};
 
 export default App;
